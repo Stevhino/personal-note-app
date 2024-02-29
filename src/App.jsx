f7da5d22e317eb3b6c5cbe18550f2acc120a68d3
Stevhino <stevpermana1@gmail.com>
2024-02-29 15:49:27 +0700

    Penambahan ThemeContext ke dalam App dan pembaruan navigasi dengan penggunaan tema dan fungsi toggle tema.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { getUserLogged, putAccessToken } from "./utils/api";
 import { LocaleProvider } from "./contexts/LocaleContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { FiMoon, FiSun } from "react-icons/fi";
+import { SiGoogletranslate } from "react-icons/si";
 import Navigation from "./components/Navigation";
 import HomePage from "./pages/HomePage";
 import AddPage from "./pages/AddPage";
@@ -31,6 +34,20 @@ class App extends React.Component {
           });
         },
       },
+      themeContext: {
+        theme: "dark",
+        toggleTheme: () => {
+          this.setState((prevState) => {
+            return {
+              themeContext: {
+                ...prevState.themeContext,
+                theme:
+                  prevState.themeContext.theme === "dark" ? "light" : "dark",
+              },
+            };
+          });
+        },
+      },
     };
 
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
@@ -46,6 +63,15 @@ class App extends React.Component {
         initializing: false,
       };
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.themeContext.theme !== this.state.themeContext.theme) {
+      document.documentElement.setAttribute(
+        "data-theme",
+        this.state.themeContext.theme
+      );
+    }
   }
 
   async onLoginSuccess({ accessToken }) {
@@ -77,52 +103,70 @@ class App extends React.Component {
     if (this.state.authedUser === null) {
       return (
         <LocaleProvider value={this.state.localeContext}>
-          <div className="app-container">
-            <header>
-              <h1>
-                <Link to="/">Aplikasi Catatan</Link>
-              </h1>
-            </header>
-            <main>
-              <Routes>
-                <Route
-                  path="/*"
-                  element={<LoginPage loginSuccess={this.onLoginSuccess} />}
-                />
-                <Route path="/register" element={<RegisterPage />} />
-              </Routes>
-            </main>
-          </div>
+          <ThemeProvider value={this.state.themeContext}>
+            <div className="app-container">
+              <header>
+                <h1>
+                  <Link to="/">
+                    {this.state.localeContext.locale === "id"
+                      ? "Aplikasi Catatan Pribadi"
+                      : "Personal Notes App"}
+                  </Link>
+                </h1>
+                <button
+                  className="toggle-locale"
+                  onClick={this.state.localeContext.toggleLocale}
+                >
+                  <SiGoogletranslate />
+                </button>
+                <button
+                  className="toggle-theme"
+                  onClick={this.state.themeContext.toggleTheme}
+                >
+                  {this.state.themeContext.theme === "dark" ? (
+                    <FiSun />
+                  ) : (
+                    <FiMoon />
+                  )}
+                </button>
+              </header>
+              <main>
+                <Routes>
+                  <Route
+                    path="/*"
+                    element={<LoginPage loginSuccess={this.onLoginSuccess} />}
+                  />
+                  <Route path="/register" element={<RegisterPage />} />
+                </Routes>
+              </main>
+            </div>
+          </ThemeProvider>
         </LocaleProvider>
       );
     }
 
     return (
       <LocaleProvider value={this.state.localeContext}>
-        <div className="app-container">
-          <header>
-            <h1>
-              <Link to="/">
-                {this.state.localeContext.locale === "id"
-                  ? "Aplikasi Catatan Pribadi"
-                  : "Personal Notes App"}
-              </Link>
-            </h1>
-            <Navigation
-              logout={this.onLogout}
-              name={this.state.authedUser.name}
-            />
-          </header>
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/notes/new" element={<AddPage />} />
-              <Route path="/archives" element={<ArchivePage />} />
-              <Route path="/notes/:id" element={<DetailPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
-        </div>
+        <ThemeProvider value={this.state.themeContext}>
+          <div className="app-container">
+            <header>
+              <Navigation
+                logout={this.onLogout}
+                name={this.state.authedUser.name}
+                toggleTheme={this.state.themeContext.toggleTheme}
+              />
+            </header>
+            <main>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/notes/new" element={<AddPage />} />
+                <Route path="/archives" element={<ArchivePage />} />
+                <Route path="/notes/:id" element={<DetailPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
       </LocaleProvider>
     );
   }
